fb155d179dd061473b12f9fcca90eee225ed739a
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-17 13:10:39 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";// treat all JS code as newer varion
+
+// alert(3 + 3) //we are using node JS not brower
+
+console.log(3 
+    + 
+     3) //code readability should be high(3 + 3)
+
+console.log("kushal")
+
+let name = "kushal"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// name => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined
+// symbol
+
+// object
+
+
+console.log(typeof "kushal")
+console.log(typeof age)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
