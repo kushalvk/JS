36ae6318ce840f193e68bf8802a0bc6c673a6f6e
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-20 07:28:59 +0000

    changes in remove coment

--- 01_basics/03_convartionOperation.js
+++ 01_basics/03_convartionOperation.js
@@ -1,14 +1,14 @@
 let d = "kushal"
 
-// console.log(typeof d);
-// console.log(typeof (d));
+console.log(typeof d);
+console.log(typeof (d));
 
 let valuinNumber5 = Number (d)
 
-// console.log(typeof valuinNumber5);
-// console.log(valuinNumber5); // NaN
+console.log(typeof valuinNumber5);
+console.log(valuinNumber5); // NaN
 
-//"33" => 33
+// "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 // null => 0
@@ -18,7 +18,7 @@ let valuinNumber5 = Number (d)
 let isLoggedIn = "kushal"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -27,35 +27,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber); // 33
-// console.log(typeof stringNumber); // string
+console.log(stringNumber); // 33
+console.log(typeof stringNumber); // string
 
 // ************************ Operations **********************
 
 let  value = 3
 let negValue = -value
-// console.log(negValue) // -3
+console.log(negValue) // -3
 
-// console.log( 2 + 2 );
-// console.log( 2 - 2 );
-// console.log( 2 * 2 );
-// console.log( 2 ** 3 );
-// console.log( 2 / 2 );
-// console.log( 2 % 3 );
+console.log( 2 + 2 );
+console.log( 2 - 2 );
+console.log( 2 * 2 );
+console.log( 2 ** 3 );
+console.log( 2 / 2 );
+console.log( 2 % 3 );
 
 let str1 = "hello"
 let str2 = " kushal"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
-// console.log("1" + 2); //12
-// console.log(1 + "2"); //12
-// console.log("1" + 2 + 2); // 122
-// console.log(1 + 2 + "2"); // 32
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
 
-// console.log(+true); // 1
-// console.log(+""); // 0
+console.log(+true); // 1
+console.log(+""); // 0
 
 let num1, num2, num3
 
--- 01_basics/04_compartions.js
+++ 01_basics/04_compartions.js
@@ -1,12 +1,12 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 <= 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
 
-// console.log("2" > 1); // 2
-// console.log("02" > 1); // 2
+console.log("2" > 1); // 2
+console.log("02" > 1); // 2
 
 
 /* avoid this type of comparotions
