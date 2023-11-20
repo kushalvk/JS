53f1d4c5052b0c268a17b67bfcf1e96e0f433372
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-20 07:09:04 +0000

    Opratores

--- 01_basics/03_convartionOperation.js
+++ 01_basics/03_convartionOperation.js
@@ -27,5 +27,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber); // 33
-console.log(typeof stringNumber); // string
+// console.log(stringNumber); // 33
+// console.log(typeof stringNumber); // string
+
+// ************************ Operations **********************
+
+let  value = 3
+let negValue = -value
+// console.log(negValue) // -3
+
+// console.log( 2 + 2 );
+// console.log( 2 - 2 );
+// console.log( 2 * 2 );
+// console.log( 2 ** 3 );
+// console.log( 2 / 2 );
+// console.log( 2 % 3 );
+
+let str1 = "hello"
+let str2 = " kushal"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // 4
+
+let gameCounter = 100
+gameCounter++; // 101
+console.log(gameCounter);
+
+let gameCounter1 = 100
+++gameCounter1; // 101
+console.log(gameCounter1);
