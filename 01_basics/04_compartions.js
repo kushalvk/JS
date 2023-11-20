7cddf2898c0203a83dc84bf40238d29214ce1501
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-20 07:25:57 +0000

    compartions

---
+++ 01_basics/04_compartions.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // 2
+// console.log("02" > 1); // 2
+
+
+/* avoid this type of comparotions
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0); // false
+console.log(undefined < 0); // false
+console.log(undefined > 0); // false
+
+*/
+
+// === 
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false (strictly check with datatype)
