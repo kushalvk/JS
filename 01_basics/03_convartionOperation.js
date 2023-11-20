18a36a6d6c8b9b656f657d1b2f7b4b13b5ff8a58
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-20 06:38:51 +0000

    Convartion of Datatype

---
+++ 01_basics/03_convartionOperation.js
@@ -0,0 +1,31 @@
+let d = "kushal"
+
+// console.log(typeof d);
+// console.log(typeof (d));
+
+let valuinNumber5 = Number (d)
+
+// console.log(typeof valuinNumber5);
+// console.log(valuinNumber5); // NaN
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+// undefaind => NaN
+// "kushal" => NaN
+
+let isLoggedIn = "kushal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "kushal" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); // 33
+console.log(typeof stringNumber); // string
