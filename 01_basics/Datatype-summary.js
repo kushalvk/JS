f8b3f951b135b8f26af0612f7b125088174adcb2
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-20 12:38:03 +0000

    datatype summary

---
+++ 01_basics/Datatype-summary.js
@@ -0,0 +1,43 @@
+// primitive datatype (call by value)
+
+// 7 type : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // let userEmail = undefined; (both are same)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId) // both are diffrent
+
+const bigNumber = 123456789n //bigInt
+
+// Reference (None primitive)
+
+    // Array, Objects, Functions
+
+    const heros = ["hanuman", "ram"]; // Array (Reference)
+
+    let myObje = {
+        name: "kushal",
+        age: 20,
+    } // Objects (Reference)
+
+    const myFunction = function(){
+        console.log("Hello world");
+    }// funcyion (Reference)
+
+console.log(typeof bigNumber); // bigint
+console.log(typeof outsideTemp); // object
+console.log(typeof score); // number
+console.log(typeof scoreValue); // number
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof userEmail); // undefined
+console.log(typeof id); // symbol
+console.log(typeof anotherId); // symbol
+
+console.log(typeof myFunction); // function
