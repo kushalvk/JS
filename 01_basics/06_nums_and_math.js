6bbb4db34256b49238ae6aecec3ba9bd3875ccba
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-21 13:10:44 +0000

    math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -17,4 +17,20 @@ const hundreds = 10000000
 
 // +++++++++++++++++++ Maths +++++++++++++++++
 
-console.log(Math); // math is object
+// console.log(Math); // math is object
+
+// console.log(Math.abs(-4)); //absolute value (only nagative change in positive)
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.2)); // 5
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.min(4,5,7,8)); //4
+// console.log(Math.max(4,5,7,8)); // 8
+
+console.log(Math.random()); // random value bitween 0 and 1
+console.log((Math.random()* 10) + 1);
+console.log(Math.floor(Math.random()* 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
