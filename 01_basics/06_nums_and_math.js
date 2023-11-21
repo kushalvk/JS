05e8bf9da757225d74ba901ea24af655ad1dce64
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-21 09:47:34 +0000

    nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,20 @@
+const score = 700
+// console.log(score);
+
+const balanbce = new Number(1400)
+// console.log(balanbce);
+
+// console.log(balanbce.toString()); // canvart in string 
+// console.log(balanbce.toString().length); // length of the string
+// console.log(balanbce.toFixed(3)); // to poine numbers
+
+const outherNumber = 23.4548
+// console.log(outherNumber.toPrecision(3)); // raund off (23.5)
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-in')); // ('en-in') is use for indian rupee
+
+
+// +++++++++++++++++++ Maths +++++++++++++++++
+
+console.log(Math); // math is object
