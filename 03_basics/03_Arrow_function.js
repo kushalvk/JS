8e009b5c930d99196583f6d63a91937cf613dc99
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-22 14:00:08 +0000

    Arrow function

---
+++ 03_basics/03_Arrow_function.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "kushal",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log((`${this.username}, welcome to website`));
+        console.log(this); // show all proparty
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "vk"
+// user.welcomeMessage()
+
+// console.log(this); // empty 
+
+// function chai(){
+//     let username = "kushal"
+//     console.log(this.username); // not work proparly
+// }
+// chai()
+
+// const chai = function () {
+//     let username = "kushal"
+//     console.log(this.username); // empty
+// }
+// chai()
+
+const chai = () => {
+    let username = "kushal"
+    console.log(this.username);
+}
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2 // ans.7
+      // same
+// const addTwo = (num1, num2) => ( num1 + num2 ) // ans.7
+
+const addTwo = (num1, num2) => ({username: "kushal"}) // to object return
+
+console.log(addTwo(3, 4));
+
+const myArray = [2, 5, 3, 7, 8]
+
