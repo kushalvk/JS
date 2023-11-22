c62c4e139c11b29376b87a5fef8537a680c8d2f3
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-11-22 09:53:58 +0000

    function1

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,7 +24,7 @@ function addTwoNumber(number1, number2) {
 }
 
 const result = addTwoNumber(3, 4)
-console.log("result: ",result);
+// console.log("result: ",result);
 
 function loginUserMessage(username = "vk"){ // if username is not defaind then bydefault usename is 'vk'
     if(!username){ // same (username === undefined) // if user name not passed
@@ -35,4 +35,32 @@ function loginUserMessage(username = "vk"){ // if username is not defaind then b
 }
 
 // console.log(loginUserMessage("kushal"))
-console.log(loginUserMessage()) // undefined just logged in
+// console.log(loginUserMessage()) // undefined just logged in
+
+function calculatCardPrice(val1, val2, ...num1) { // '...' in array
+    return num1
+}
+
+// console.log(calculatCardPrice(200, 500, 300, 2000));
+
+const user = {
+    username: "kushal",
+    prices: 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "vk",
+    price: 400
+})
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(gerArray) {
+    return gerArray[1]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
