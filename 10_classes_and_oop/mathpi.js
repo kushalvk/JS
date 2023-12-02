03b5fb65e9e40336e8ca0ab91da6711f13dd0066
kushalvk <142973474+kushalvk@users.noreply.github.com>
2023-12-02 07:35:17 +0000

    math

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,30 @@
+const decripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(decripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI); // PI value can't be overwrite
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailavle: true,
+
+    orderChai: function () {
+        console.log("Chai nahi bani");
+    }
+}
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    // writable: false,
+    enumerable: false // loops cannot be access on false enumerable (only name)
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (const [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key} : ${value}`);
+    }
+}
